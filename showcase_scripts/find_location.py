29ab153a7bee204e7d6152e98da256dff0840909
Nick <nicholas.j.thompson-1@ou.edu>
2022-05-01 13:54:52 -0500

    implement the table

--- showcase_scripts/find_location.py
+++ showcase_scripts/find_location.py
@@ -6,7 +6,7 @@ import io
 
 from google.cloud import vision
 
-IMAGE_PATH = 'dataset/1 degree to right.jpg'
+IMAGE_PATH = 'dataset/original2.jpg'
 
 def assemble_word(word):
     assembled_word=""
@@ -56,9 +56,25 @@ def main():
 
     width, height = img.size
 
+    # fix tilt
+    # img0 = img.crop((0, 0, width/3, height/8))
+    img0 = img.crop((0,0, width, height))
+    buffer = io.BytesIO()
+    img0.save(buffer, "PNG")
+
+    content = buffer.getvalue()
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    vertices = texts[2].bounding_poly.vertices
+    tilt_angle = math.atan2(vertices[3].y-vertices[0].y,vertices[0].x-vertices[3].x) * (180/math.pi)
+    img = img.rotate(90-tilt_angle)
 
 
-    #image with NPI
+
+    # image with NPI
     img1 = img.crop((width/1.5, height/7, width, height/5))
     buffer1 = io.BytesIO()
     img1.save(buffer1, "PNG")
@@ -70,32 +86,96 @@ def main():
     response1 = client.document_text_detection(image=image1)
     document1 = response1.full_text_annotation
 
-    location_npi = find_word_location(document1, 'NPI')
-    tilt_angle = math.atan2(location_npi.vertices[3].y-location_npi.vertices[0].y,location_npi.vertices[0].x-location_npi.vertices[3].x) * (180/math.pi)
-    img.show()
-    img = img.rotate(90-tilt_angle)
-    img.show()
-    print(location_npi)
-    npi_text = text_within(document1, location_npi.vertices[1].x, location_npi.vertices[1].y, (width/8) + location_npi.vertices[1].x, 10+location_npi.vertices[2].y)
+
+
+    # location_npi = find_word_location(document1, 'NPI')
+    # npi_text = text_within(document1, 15+location_npi.vertices[1].x, -5+location_npi.vertices[1].y, 245 + location_npi.vertices[1].x, 5+location_npi.vertices[2].y)
     print(npi_text)
 
 
-    #image with main form data
-    img2 = img.crop((0, height/4, width, height/2))
-    buffer2 = io.BytesIO()
-    img2.save(buffer2, "PNG")
+    #image with header
+    # img2 = img.crop((0, height/3.1, width, height/2.7))
+    # buffer2 = io.BytesIO()
+    # img2.save(buffer2, "PNG")
 
-    content2 = buffer2.getvalue()
+    # content2 = buffer2.getvalue()
 
-    image2 = vision.Image(content=content2)
+    # image2 = vision.Image(content=content2)
     
 
-    response2 = client.document_text_detection(image=image2)
-    document2 = response2.full_text_annotation
+    # response2 = client.document_text_detection(image=image2)
+    # document2 = response2.full_text_annotation
 
-    location_plan = find_word_location(document2, 'PLAN')
-    plan_text = text_within(document2, 10+location_plan.vertices[1].x, -15+location_plan.vertices[1].y, 800+location_plan.vertices[1].x, 10+location_plan.vertices[2].y)
-    print(plan_text)
+    # location_plan = find_word_location(document2, 'PLAN')
+    # plan_text = text_within(document2, 10+location_plan.vertices[1].x, -5+location_plan.vertices[1].y, 800+location_plan.vertices[1].x, 5+location_plan.vertices[2].y)
+    # print(plan_text)
+
+    # location_client = find_word_location(document2, 'ID')
+    # client_id = text_within(document2, 15+location_client.vertices[1].x, -5+location_client.vertices[1].y, 200+location_client.vertices[1].x, 5+location_client.vertices[2].y)
+    # client_name = text_within(document2, 200+location_client.vertices[1].x, -5+location_client.vertices[1].y, 900+location_client.vertices[1].x, 5+location_client.vertices[2].y)
+    # print(client_id)
+    # print(client_name)
+
+    # location_product = find_word_location(document2, 'PRODUCT')
+    # product = text_within(document2, 15+location_product.vertices[1].x, -5+location_product.vertices[1].y, 900+location_product.vertices[1].x, 5+location_product.vertices[2].y)
+    # print(product)
+
+    # location_subclient = find_word_location(document2, 'SUBCLIENT')
+    # subclient_id = text_within(document2, 15+location_subclient.vertices[1].x, -5+location_subclient.vertices[1].y, 200+location_subclient.vertices[1].x, 5+location_subclient.vertices[2].y)
+    # subclient_name = text_within(document2, 200+location_subclient.vertices[1].x, -5+location_subclient.vertices[1].y, 900+location_subclient.vertices[1].x, 5+location_subclient.vertices[2].y)
+    # print(subclient_id)
+    # print(subclient_name)
+
+    # img3 = img.crop((0, height/2.7, width, height/2.6))
+    # buffer3 = io.BytesIO()
+    # img3.save(buffer3, "PNG")
+
+    # content3 = buffer3.getvalue()
+
+    # image3 = vision.Image(content=content3)
+    
+
+    # response3 = client.document_text_detection(image=image3)
+    # document3 = response3.full_text_annotation
+
+    # location_network = find_word_location(document3, 'NETWORK')
+    # network = text_within(document3, 15+location_network.vertices[1].x, -5+location_network.vertices[1].y, 400+location_network.vertices[1].x, 5+location_network.vertices[2].y)
+    # print(network)
+
+    # image of form data
+    index = 0
+    table_data = []
+    while True:
+        img4 = img.crop((0, height/2.55+index, width, height/2.45+index))
+        buffer4 = io.BytesIO()
+        img4.save(buffer4, "PNG")
+        img4.show()
+
+        content4 = buffer4.getvalue()
+
+        image4 = vision.Image(content=content4)
+
+        response4 = client.document_text_detection(image=image4)
+        document4 = response4.full_text_annotation
+
+        if document4.text == '':
+            break
+        else:
+            text = document4.text.replace(' ', '\n')
+            table_data.append(text.split())
+
+            index += 50
+
+    print(table_data)
+    print('eef')
+
+
+
+
+
+         
+
+
 
 
 if __name__ == "__main__":
